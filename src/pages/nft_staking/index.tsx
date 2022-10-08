a5f839fa8ebfca9b68a8d3b595605200d92a7967
fashion-cstar <ted228.dev@gmail.com>
2022-10-08 10:38:47 +0800

    fix mobile responsive on coming soon page

--- src/pages/nft_staking/index.tsx
+++ src/pages/nft_staking/index.tsx
@@ -2,8 +2,7 @@
 import React, { useState } from 'react'
 import { useNFT } from 'src/contexts'
 
-export const NFTStaking = () => {
-    const { tokenPerMint, bnbPerMint } = useNFT()
+export const NFTStaking = () => {    
 
     return (
         <div className="w-full">
@@ -31,7 +30,7 @@ export const NFTStaking = () => {
                 <div className='w-full flex justify-center'>
                     <div className='max-w-[1440px] w-full px-6 md:px-8'>
                         <div className='w-full text-[22px] md:text-[25px] text-[#000] font-semibold my-6 md:my-8' style={{ fontFamily: 'Bebas' }}>Stake your 'Mad Scientist' NFTs to earn BNB for staking!</div>
-                        <div className='w-full flex flex-col gap-6 text-[25px] text-[#000]'>
+                        <div className='w-full flex flex-col gap-6 text-[25px] text-[#000] hidden md:flex'>
                             <div className=''>Click <span className='text-[#7F41E4]'>"Approve Staking“</span>, so your address is eligible to stake.</div>
                             <span className='font-semibold' style={{ fontFamily: 'Bebas' }}>
                                 This requires a BSC transaction and a one time gas fee.
@@ -42,12 +41,25 @@ export const NFTStaking = () => {
                         </div>
                     </div>
                 </div>
-                <div className='w-full bg-app-purple py-6 md:py-8 my-10 flex  flex-col items-center gap-4'>
+                <div className='w-full bg-app-purple py-6 md:py-8 my-8 md:my-10 flex  flex-col items-center gap-4'>
                     <div className='text-[25px] text-white uppercase text-center' style={{ textShadow: '2px 2px #000000' }}>
                         access to stake dashboard:
                     </div>
                     <div className='bg-white rounded-tl-lg rounded-tr-sm rounded-bl-sm rounded-br-lg mx-6 px-8 py-2 max-w-[400px] text-[25px] md:text-[30px] text-center text-[#6FFF39] uppercase' style={{ boxShadow: '3px 3px #000' }}>
                         Coming soon
+                    </div>
+                </div>
+                <div className='w-full flex justify-center md:hidden'>
+                    <div className='max-w-[1440px] w-full px-6 md:px-8'>                        
+                        <div className='w-full flex flex-col gap-6 text-[25px] text-[#000]'>
+                            <div className=''>Click <span className='text-[#7F41E4]'>"Approve Staking“</span>, so your address is eligible to stake.</div>
+                            <span className='font-semibold' style={{ fontFamily: 'Bebas' }}>
+                                This requires a BSC transaction and a one time gas fee.
+                                Select the NFTs you want to stake on the left.
+                                Click "Stake Selected NFTs" and approve the transaction.
+                                Sit back and earn BNB every day.
+                            </span>
+                        </div>
                     </div>
                 </div>
                 <div className='h-[40px]' />
